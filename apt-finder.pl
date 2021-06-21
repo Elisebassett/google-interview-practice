3710b6a45bd7fb6228bc6c7bf2a7958e8bc10373
Elise Bassett <elisebassett@gmail.com>
2021-06-21 13:23:46 -0600

    Test file setup for perl

--- apt-finder.pl
+++ apt-finder.pl
@@ -2,4 +2,23 @@
 use strict;
 use warnings;
 
-print "Hello World! Also other stuff";
+sub apt_finder {
+    my (@reqs, @blocks) = shift;
+    #Check block 
+    for my $block (@blocks) {
+        $block{@reqs}
+    }
+    print "Hello World! Also other stuff";
+}
+
+my %functions = (
+    apt_finder => \&apt_finder,
+);
+
+my $function = shift;
+
+if (exists $functions{$function}) {
+    $functions{$function}->();
+} else {
+    die "There is no function called $function available\n";
+}
---
+++ tests/apt-finder.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+plan tests => 3;
+
+ok(1);
